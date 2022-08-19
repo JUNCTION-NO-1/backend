9debf8a2604b9e3a77825ddbb69072c361f51567
최승원 <alxndr@marketboro.com>
2022-08-20 02:10:17 +0900

    first commit

---
+++ src/test/java/me/alxndr/junction/JunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package me.alxndr.junction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
